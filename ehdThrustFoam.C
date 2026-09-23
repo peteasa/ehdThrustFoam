3f8f00cb067c816285f81f15abddf89efe4b999c
Peter Saunderson <peteasa@gmail.com>
2026-09-23 17:10:40 +0100

    improve Pphys clipping

--- ehdThrustFoam.C
+++ ehdThrustFoam.C
@@ -112,7 +112,7 @@ int main(int argc, char *argv[])
     int pwr = -2;
     scalar factor = factMulti * pow(10.0, pwr);
     bool factorChange = false;
-    scalar factorCh_old = factorChange;
+    bool factorCh_old = factorChange;
 
     /*************************************************************************/
     // startup time - separate electrostatic and convection interaction
@@ -221,7 +221,8 @@ int main(int argc, char *argv[])
         int iterPerLogs = 1000;
         int enableDetailedLogs = !(runTime.timeIndex() % iterPerLogs);
         if (!(intervalCount % maxInterval) || runTime.timeIndex() < 10 || runTime.deltaTValue() < 1e-40
-            || (0.01 < startupLPFrac && startupLPFrac < 0.015) || (0.95 < startupLPFrac && startupLPFrac < 0.99 ))
+            || (startupPHalfwidth < startupLPFrac && startupLPFrac < startupPHalfwidth + 0.005)
+            || (runLPFrac - 0.005 < startupLPFrac && startupLPFrac < runLPFrac ))
         {
             enableDetailedLogs = true;
         }
